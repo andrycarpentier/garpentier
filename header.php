79dbffae9b3137bf20e329a29eaf9b67a08cfbd8
unknown <andry.caprentier@gmail.com>
2017-09-25 14:16:15 +0200

    J'ai rajouté les fichiers footer, header, index, screenshot, style

---
+++ header.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title> <?php bloginfo('name'); ?> </title>
+    <?php wp_head(); ?>
+  </head>
+
+  <body>
+    <header>
+      <h1>Salut à tous les amis, c'est David Lafarge Pokémon</h1>
+      <h2><?php bloginfo('description'); ?></h2>
+    </header>
